3dd45fb0988c1ce8ea614f49d045af06a7a5389e
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-27 21:49:25 +0900

    최근후기 프래그먼트 작업  - Homefragment.xml에 -> HomeReviewFragment 배치

---
+++ app/src/main/java/com/start/mindcafeclonepractice/bottomnavhomefragment/HomeReviewFragment.kt
@@ -0,0 +1,35 @@
+package com.start.mindcafeclonepractice.bottomnavhomefragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.start.mindcafeclonepractice.R
+import com.start.mindcafeclonepractice.bottomnavfragments.BaseFragment
+import com.start.mindcafeclonepractice.databinding.FragmentHomeReviewBinding
+
+class HomeReviewFragment: BaseFragment() {
+
+    lateinit var binding: FragmentHomeReviewBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_review, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
