e0a549d2685b5208d4c2f6bd3d9213c846adc277
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-27 15:16:23 +0900

    HomeFragment -> 그룹프로그램프래그먼트 배치  - 그룹프로그램클릭 -> AlertDialog활용

--- app/src/main/java/com/start/mindcafeclonepractice/bottomnavhomefragment/HomeGroupProgramFragment.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/bottomnavhomefragment/HomeGroupProgramFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.start.mindcafeclonepractice.R
 import com.start.mindcafeclonepractice.bottomnavfragments.BaseFragment
@@ -29,6 +30,17 @@ class HomeGroupProgramFragment: BaseFragment() {
     }
 
     override fun setupEvents() {
+        binding.btnGroupProgramInfo.setOnClickListener {
+
+            val alert = AlertDialog.Builder(mContext)
+                alert.setTitle("그룹프로그램이란?")
+                alert.setMessage("그룹 테라피와 그룹 코칭이 통합 된 소규모 그룹(4~10명)으로 진행되는 프로그램입니다.\n" +
+                        "그룹프로그램을 이끄는 그룹 리더와 비슷한 고민이나 고통을 겪는 참가자들로 구성됩니다." +
+                        "\n\n1.그룹 테라피\n상담사가 리더이고 심리치유를 목적으로 하는 그룹 테라피\n\n2. 그룹코칭" +
+                        "\n코치가 리더이고 꿈과 목표를 목적으로 하는 그룹테라피")
+                alert.setPositiveButton("확인",null)
+                alert.show()
+        }
 
     }
 
