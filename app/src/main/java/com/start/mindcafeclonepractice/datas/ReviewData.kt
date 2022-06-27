849c2bcc315e06b2764feeb55624091bc0b7050d
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-27 21:17:28 +0900

    최근후기 프래그먼트 작업  - recyclerview, list_item.xml, 리뷰데이타클래스

---
+++ app/src/main/java/com/start/mindcafeclonepractice/datas/ReviewData.kt
@@ -0,0 +1,12 @@
+package com.start.mindcafeclonepractice.datas
+
+data class ReviewData(
+    val coachName: String? = null,
+    val profileImg: String? = null,
+    val purchaseProduct: String? = null,
+    val solutionImg: String? = null,
+    val content: String? = null,
+    val ratingImg: String? = null,
+    val ratingNum: Float? = null
+) {
+}
