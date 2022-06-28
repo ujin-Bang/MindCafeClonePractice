12ce31f3d5bd4d8bef350bbbebfc639b424902f0
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-28 17:04:07 +0900

    신규전문가 리스트item.xml + data클래스 생성

---
+++ app/src/main/java/com/start/mindcafeclonepractice/datas/NewExpertData.kt
@@ -0,0 +1,10 @@
+package com.start.mindcafeclonepractice.datas
+
+class NewExpertData(
+    val profileImg: String? = null,
+    val name: String? = null,
+    val introduction: String? = null,
+    val consultingTool1: String? = null,
+    val consultingTool2: String? = null
+) {
+}
