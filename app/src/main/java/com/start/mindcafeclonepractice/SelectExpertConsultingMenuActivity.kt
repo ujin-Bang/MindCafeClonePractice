b62d34ae02c2fb068688e557f940e0125dd283cc
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-02 18:46:31 +0900

    전문가 선택버튼 클릭 -> 전문가 상담 메뉴 소개  - 뷰페이저2 + 텝레이아웃 연결  -  TabLayoutMediator(binding.consultingMenuTabLayout, binding.consultingViewPager){tab, position ->
                   when(position) {
                       0 -> tab.text = "채팅"
                       else -> tab.text = "전화"
                   }
    
               }.attach()
    
     - 텝레이아웃 인디케이터 글씨 크기와 같게/ 글씨 색깔/ 인디케이터 색깔 설정

--- app/src/main/java/com/start/mindcafeclonepractice/SelectExpertConsultingMenuActivity.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/SelectExpertConsultingMenuActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
+import com.google.android.material.tabs.TabLayoutMediator
 import com.start.mindcafeclonepractice.adapters.ConsultingMenuViewPagerAdapter
 import com.start.mindcafeclonepractice.databinding.ActivitySelectExpertConsultingMenuBinding
 import com.start.mindcafeclonepractice.datas.NewExpertData
@@ -34,8 +35,20 @@ class SelectExpertConsultingMenuActivity : BaseActivity() {
 
         actionBarCustom()
 
+        //뷰페이저 어댑터에 내가 만든 어댑터 연결
         mConsultingMenuAdapter = ConsultingMenuViewPagerAdapter(mContext as FragmentActivity)
         binding.consultingViewPager.adapter = mConsultingMenuAdapter
+
+        //뷰페이져 + 텝레이아웃 연결
+        TabLayoutMediator(binding.consultingMenuTabLayout, binding.consultingViewPager){tab, position ->
+
+            when(position) {
+                0 -> tab.text = "채팅"
+                else -> tab.text = "전화"
+            }
+
+        }.attach()
+
 
     }
 
