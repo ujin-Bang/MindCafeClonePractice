814e629a412c78f9fb9a12a730cf1762199b35a6
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-05 19:58:47 +0900

    마인드 포스팃 페이지 글쓰기 작업  - alertDialog 커스텀 띄우기  - xml로 커스텀할 모양 그리기  - aletDialog.dismiss() : 완료버튼 누를시 얼럿창 닫힘효과

--- app/src/main/java/com/start/mindcafeclonepractice/MindPostitActivity.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/MindPostitActivity.kt
@@ -2,17 +2,44 @@ package com.start.mindcafeclonepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.databinding.DataBindingUtil
+import com.start.mindcafeclonepractice.databinding.ActivityMindPostitBinding
 
 class MindPostitActivity : BaseActivity() {
+
+    lateinit var binding: ActivityMindPostitBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mind_postit)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_mind_postit)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+
+        binding.btnPostItWright.setOnClickListener {
+
+        val dialogCustom = LayoutInflater.from(mContext).inflate(R.layout.alert_dialog_custom,null)
+        val alertDialog = AlertDialog.Builder(mContext)
+            .setView(dialogCustom)
+            .create()
+
+        val inputContent = dialogCustom.findViewById<EditText>(R.id.edtContent).text
+        val btnOk = dialogCustom.findViewById<TextView>(R.id.btnOk)
+
+            btnOk.setOnClickListener {
+                alertDialog.dismiss()
+                Log.d("입력값","content: ${inputContent}")
+            }
+            alertDialog.show()
+
+        }
 
     }
 
