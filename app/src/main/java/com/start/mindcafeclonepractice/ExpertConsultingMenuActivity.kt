fd2081887c47fb427db82528f00fa8fdbdf3cefa
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-08 12:27:27 +0900

    전문가 컨설팅 메뉴화면 - 보낸 데이터 활용 액션바 해당상담사 이름으로 변경

--- app/src/main/java/com/start/mindcafeclonepractice/ExpertConsultingMenuActivity.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/ExpertConsultingMenuActivity.kt
@@ -1,13 +1,16 @@
 package com.start.mindcafeclonepractice
 
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.start.mindcafeclonepractice.databinding.ActivityExpertConsultingMenuactivityBinding
 import com.start.mindcafeclonepractice.databinding.ActivitySelectExpertConsultingMenuBinding
+import com.start.mindcafeclonepractice.datas.ReviewData
 
 class ExpertConsultingMenuActivity : BaseActivity() {
 
     lateinit var binding: ActivityExpertConsultingMenuactivityBinding
+    lateinit var mData: ReviewData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,12 @@ class ExpertConsultingMenuActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mLinearLayoutMainActionBar.visibility = View.GONE
+        mLinearLayoutNewExpertSelectedConsultingMenu.visibility = View.VISIBLE
+        mData = intent.getSerializableExtra("data") as ReviewData
+
+        mSelectedExpertName.text = mData.coachName
 
     }
 }
