9c31a845765e575cd77ba589fb13b8755ee2060d
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-08 21:28:30 +0900

    전문가 컨설팅 메뉴화면 - 전화탭 리싸이클러뷰 + 파이어베이스 데이터 연결  - 실제 데이터 입력 => 두가지 데이터는 입력안됨..왜? 널포인트익셉션 오류

--- app/src/main/java/com/start/mindcafeclonepractice/adapters/ExpertConsultingMenuPhoneRecyclerAdapter.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/adapters/ExpertConsultingMenuPhoneRecyclerAdapter.kt
@@ -1,6 +1,8 @@
 package com.start.mindcafeclonepractice.adapters
 
 import android.content.Context
+import android.os.Parcel
+import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +10,6 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.start.mindcafeclonepractice.R
 import com.start.mindcafeclonepractice.datas.ExpertConsultingMenuPhoneData
-import org.w3c.dom.Text
 
 class ExpertConsultingMenuPhoneRecyclerAdapter (
     val mContext: Context,
@@ -16,30 +17,30 @@ class ExpertConsultingMenuPhoneRecyclerAdapter (
         ):RecyclerView.Adapter<ExpertConsultingMenuPhoneRecyclerAdapter.PhoneViewHolder>() {
 
     inner class PhoneViewHolder(row: View) : RecyclerView.ViewHolder(row){
+
         val txtTitle = row.findViewById<TextView>(R.id.txtTitle)
         val txtPrice = row.findViewById<TextView>(R.id.txtPrice)
-        val txtTimeMinutes = row.findViewById<TextView>(R.id.txtTimeMinutes)
-        val txtExpiration = row.findViewById<TextView>(R.id.txtExpiration)
+        val txtTimeMinutes = row.findViewById<TextView>(R.id.txtTimeMinutess)
+        val txtExpiration = row.findViewById<TextView>(R.id.txtExpirations)
 
         fun bind(data: ExpertConsultingMenuPhoneData){
             txtTitle.text = data.title
-            txtExpiration.text = data.expiration
-            txtPrice.text = data.price.toString()
-            txtTimeMinutes.text = data.timeMinutes
+            txtPrice.text = data.price?.let { data.getFormattedPrice(it) }
         }
     }
 
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhoneViewHolder {
 
-        val span = LayoutInflater.from(mContext).inflate(R.layout.fragment_expert_consulting_menu_phone,parent,false)
-        return PhoneViewHolder(span)
+        val row = LayoutInflater.from(mContext).inflate(R.layout.consulting_menu_phone_list_item,parent,false)
+        return PhoneViewHolder(row)
     }
 
     override fun onBindViewHolder(holder: PhoneViewHolder, position: Int) {
 
         holder.bind(mList[position])
-
     }
 
     override fun getItemCount() = mList.size
+
 }
--- app/src/main/java/com/start/mindcafeclonepractice/expertconsultinmenuviewpagerfragment/ExpertConsultingMenuPhoneFragment.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/expertconsultinmenuviewpagerfragment/ExpertConsultingMenuPhoneFragment.kt
@@ -1,17 +1,28 @@
 package com.start.mindcafeclonepractice.expertconsultinmenuviewpagerfragment
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.*
 import com.start.mindcafeclonepractice.R
+import com.start.mindcafeclonepractice.adapters.ExpertConsultingMenuPhoneRecyclerAdapter
 import com.start.mindcafeclonepractice.bottomnavfragments.BaseFragment
 import com.start.mindcafeclonepractice.databinding.FragmentExpertConsultingMenuPhoneBinding
+import com.start.mindcafeclonepractice.datas.ExpertConsultingMenuPhoneData
 
 class ExpertConsultingMenuPhoneFragment:BaseFragment() {
 
     lateinit var binding: FragmentExpertConsultingMenuPhoneBinding
+    val mVoiceCoachList= ArrayList<ExpertConsultingMenuPhoneData>()
+    lateinit var mAdapter: ExpertConsultingMenuPhoneRecyclerAdapter
+
+    var ref: DatabaseReference? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +30,9 @@ class ExpertConsultingMenuPhoneFragment:BaseFragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_expert_consulting_menu_phone, container, false)
+
+       getPhoneTabDataFromFirebase()
+
         return binding.root
     }
 
@@ -36,4 +50,36 @@ class ExpertConsultingMenuPhoneFragment:BaseFragment() {
     override fun setValues() {
 
     }
+
+    fun getPhoneTabDataFromFirebase(){
+        ref = FirebaseDatabase.getInstance().getReference("review").child("1").child("phoneCoaching")
+        ref?.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    for (h in snapshot.children){
+                        val phoneData = h.getValue(ExpertConsultingMenuPhoneData::class.java)
+                        if (phoneData != null) {
+                            mVoiceCoachList.add(phoneData)
+                            Log.d("파이어베이스데이터 추가후 메시지",phoneData.toString())
+                        }
+
+                    }
+                    mAdapter.notifyDataSetChanged()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+                Toast.makeText(mContext, error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+        mAdapter = ExpertConsultingMenuPhoneRecyclerAdapter(mContext,mVoiceCoachList)
+        binding.consultingMenuPhoneRecyclerView.adapter = mAdapter
+        binding.consultingMenuPhoneRecyclerView.layoutManager = LinearLayoutManager(mContext)
+        binding.consultingMenuPhoneRecyclerView.setHasFixedSize(true)
+
+
+    }
+
 }
