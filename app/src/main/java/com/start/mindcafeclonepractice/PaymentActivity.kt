7571e6eb9f4a44481cc9483c830419b2009fa9a1
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-11 16:51:56 +0900

    PaymentActivity 페이지 작업중 - 쿠폰등록하기 클릭시 => aletDialog띄우기 까지만  - 커스텀 다이얼로그 xml만들기 + 연결까지만

--- app/src/main/java/com/start/mindcafeclonepractice/PaymentActivity.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/PaymentActivity.kt
@@ -2,10 +2,13 @@ package com.start.mindcafeclonepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.start.mindcafeclonepractice.adapters.ExpertConsultingMenuChattRecyclerAdapter
@@ -28,7 +31,37 @@ class PaymentActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        spinnerClicked()
+
+        binding.btnCouponRegistration.setOnClickListener {
+
+            val customDialog = LayoutInflater.from(mContext).inflate(R.layout.coupon_registration_custom_alert,null)
+            val alert = AlertDialog.Builder(mContext)
+                .setView(customDialog)
+                .create()
+                alert.show()
+
+        }
+
+
+
+    }
+
+    override fun setValues() {
+        mData = intent.getSerializableExtra("data") as ExpertConsultingMenuChattData
+
+        mLinearLayoutMainActionBar.visibility = View.GONE
+        mLinearLayoutPaymentActionBar.visibility = View.VISIBLE
+        mTxtPaymentTitle.text = mData.title
+    }
+
+
+    fun spinnerClicked(){
+
+        //어댑터 활용 spinner에 들어갈 아이템리스트 목록 만들고 스피너에 연결 -스피너 커스텀하기
         val customList = arrayListOf("쿠폰선택","10% 할인","20% 할인","여름 방학 할인","주중 할인","공휴일 할인","1년 할인")
+
+        //support_simple_spinner_dropdouw_item => 기본스피너 모양 안드로이드에서 제공
         val adapter = ArrayAdapter<String>(mContext, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,customList)
         binding.couponSpinner.adapter = adapter
 
@@ -40,13 +73,7 @@ class PaymentActivity : BaseActivity() {
                 id: Long
             ) {
 
-                if(position == 0 ){
-                    Toast.makeText(mContext, "쿠폰을 선택해 주세요.", Toast.LENGTH_SHORT).show()
-
-                }
-                else{
-                    Toast.makeText(mContext, "${parent?.selectedItem.toString()}쿠폰선택, 위치: ${position}", Toast.LENGTH_SHORT).show()
-                }
+                Log.d("스피너아이템선택","위치 : ${position}번째")
 
             }
 
@@ -55,14 +82,5 @@ class PaymentActivity : BaseActivity() {
             }
 
         }
-
-    }
-
-    override fun setValues() {
-        mData = intent.getSerializableExtra("data") as ExpertConsultingMenuChattData
-
-        mLinearLayoutMainActionBar.visibility = View.GONE
-        mLinearLayoutPaymentActionBar.visibility = View.VISIBLE
-        mTxtPaymentTitle.text = mData.title
     }
 }
