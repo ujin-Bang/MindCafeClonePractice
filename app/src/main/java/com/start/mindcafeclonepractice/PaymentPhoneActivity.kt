3a868f09a9e7e0ce33ee0c2190c9d0355665a71f
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-12 20:56:18 +0900

    전화탭 결제 화면 액티비티에서 - 스피너 작업 : 안드로이드에서 주는 기본 스피너 사용 => 리스트 + 어댑터 구현(리스트뷰 사용법과 비슷함)

--- app/src/main/java/com/start/mindcafeclonepractice/PaymentPhoneActivity.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/PaymentPhoneActivity.kt
@@ -1,7 +1,11 @@
 package com.start.mindcafeclonepractice
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.start.mindcafeclonepractice.databinding.ActivityPaymentPhoneBinding
 import com.start.mindcafeclonepractice.datas.ExpertConsultingMenuPhoneData
@@ -20,6 +24,7 @@ class PaymentPhoneActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        spinner()
     }
 
     override fun setValues() {
@@ -30,5 +35,34 @@ class PaymentPhoneActivity : BaseActivity() {
         mData = intent.getSerializableExtra("data") as ExpertConsultingMenuPhoneData
         mTxtPaymentPhoneTitle.text = mData.title
 
+    }
+
+    fun spinner(){
+        val spinnerList = arrayListOf("쿠폰을 선택해주세요.","10% 매달 첫째주 할인","20% 방학할인","10% 생일맞이 할인","30% 개근상 할인","10% 가족할인","10% 친구추천 할인")
+        val spAdapter = ArrayAdapter<String>(mContext, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,spinnerList)
+        binding.phoneCouponSpinner.adapter = spAdapter
+
+
+        binding.phoneCouponSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+
+               if (parent != null){
+
+                   Log.d("스피너 아이템 선택: ", "${parent.selectedItem}을 선택했습니다..")
+//                    Toast.makeText(mContext, "${parent.selectedItem} ", Toast.LENGTH_SHORT).show()
+              }
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
     }
 }
