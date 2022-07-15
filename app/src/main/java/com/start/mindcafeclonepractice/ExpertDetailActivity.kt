fb1ece4cf6a70c6517ed210bc3693a8fef745458
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-07-15 20:19:20 +0900

    ExpertDetailActivity(전문가프로필)에서 작업중 - 상속받은 액션바 수정, 받아온 데이터 반영

--- app/src/main/java/com/start/mindcafeclonepractice/ExpertDetailActivity.kt
+++ app/src/main/java/com/start/mindcafeclonepractice/ExpertDetailActivity.kt
@@ -1,13 +1,16 @@
 package com.start.mindcafeclonepractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.start.mindcafeclonepractice.databinding.ActivityExpertDetailBinding
+import com.start.mindcafeclonepractice.datas.CoachData
 
 class ExpertDetailActivity : BaseActivity() {
 
     lateinit var binding: ActivityExpertDetailBinding
+    lateinit var mData: CoachData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,5 +25,14 @@ class ExpertDetailActivity : BaseActivity() {
 
     override fun setValues() {
 
+        mData = intent.getSerializableExtra("data") as CoachData
+
+        mLinearLayoutMainActionBar.visibility = View.GONE
+        mLinearLayoutPaymentActionBar.visibility = View.VISIBLE
+
+        mTxtPaymentTitle.text = "                     전문가 프로필"
+        Glide.with(mContext).load(mData.profileImg).into(binding.imgExpertPhoto)
+        binding.txtExpertName.text = mData.name
+        binding.txtExertIntroduction.text = mData.introduction
     }
 }
